8e68908a730a51393fe80f710a993bc88dd4289d
xzjqx <xzjqx@outlook.com>
2019-11-23 16:47:09 +0800

    Site updated: 2019-11-23 16:47:05

---
+++ love/love.c
@@ -0,0 +1,33 @@
+/*
+	I meet with you in junior high school,
+	fall in love with you in high school,
+	thorough understand with you in collage school.
+	So, I decided to express my love with this web.
+*/
+
+Boy i = xzjqx;
+Girl u = qianqian;
+Pair we = u and i;
+void Past() {
+	i.express(my love);
+	// luckily
+	u.accept(i);
+	// after high school, we were broke up.
+	u.break_up_with(i);
+}
+void Now() {
+	i.fall_in_love_with(u);
+	// I am sincere.
+	i.sincerely;
+}
+void Future() {
+	if u.accept(i)
+		we.together;
+	else
+		we.good_friends;
+}
+int main() {
+	Past();
+	Now();
+	Future();
+}
